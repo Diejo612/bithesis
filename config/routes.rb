78f1b70dd1c11eef9f9b019ecdcfa8a98a222c9d
Jean Pier <diegojeanpier.dc.@gmail.com>
2022-02-02 15:55:31 -0500

    terminado, rutas, dashboard, navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/resources', to: "pages#resources", as: 'resources'
   resources :lines, only: [:index]
+  resources :interviews, only: [ :new, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
