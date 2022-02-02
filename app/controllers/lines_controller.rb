e6c346683bb22943b668c53626d2322ed9f9ff25
Jean Pier <diegojeanpier.dc.@gmail.com>
2022-02-02 17:15:16 -0500

    navbar a medias con el avatar

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -2,7 +2,9 @@ class LinesController < ApplicationController
 
   def index
     @lines = Line.all
-    @interview = Interview.where(user: current_user).last
-    @date = @interview.date
+    unless current_user.role == 'teacher'
+      @interview = Interview.where(user: current_user).last
+      @date = @interview.date
+    end
   end
 end
