20893fbb59e3b6ac42fe1ed7ce2e95e02ea78fb9
mel <mel.valle.perez2003@gmail.com>
2022-02-03 11:10:34 -0500

    avance articulo

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ puts 'Creando avatar'
 avatar = URI.open('https://images.unsplash.com/photo-1631203928493-a4e4eb2b8da1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80')
 
 puts 'Creando Usuarios'
-User.create(first_name: 'María', last_name: 'Henriquez', role: 'teacher', email: 'maria@email.com', password: '1234567').photo.attach(io: avatar, filename: 'avatar.png', content_type: 'image/png');
+User.create(first_name: 'María', last_name: 'Henriquez', role: 'teacher', email: 'maria@email.com', password: '1234567')
 User.create(first_name: 'Mel', last_name: 'Valle', role: 'student', email: 'mel@email.com', password: '1234567')
 User.create(first_name: 'Jhon', last_name: 'Figueroa', role: 'student', email: 'jh@email.com', password: '1234567')
 User.create(first_name: 'Jean', last_name: 'Pier', role: 'student', email: 'jean_pier@email.com', password: '1234567')
