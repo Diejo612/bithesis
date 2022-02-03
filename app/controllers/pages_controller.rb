fe2d1092e3fb0cd95c3e1f0a53e217392eafa56f
Jean Pier <diegojeanpier.dc.@gmail.com>
2022-02-03 16:24:34 -0500

    permiso para articulo1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :recursos ]
+  skip_before_action :authenticate_user!, only: [ :home, :recursos, :articulo1 ]
 
   def home
   end
