b3e32ae9b0d111ae41f32ce33f0537685faf0434
Jean Pier <diegojeanpier.dc.@gmail.com>
2022-02-05 18:27:08 -0500

    cambio en íconos

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,9 +1,11 @@
 class LinesController < ApplicationController
 
   def index
+    @lines_all = Line.all
     if current_user.assignation
       @lines = current_user.assignation.map { |a| a.line }
     end
+    @active_line = @lines.first.id - 1
     unless current_user.role == 'teacher'
       @interview = Interview.where(user: current_user).last
       if @interview
