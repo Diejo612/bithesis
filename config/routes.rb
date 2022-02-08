27e2754f80ed018b60902e2148a641147ca93554
mel <mel.valle.perez2003@gmail.com>
2022-02-08 10:42:00 -0500

    calendario arreglado

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   get '/recursos', to: "pages#recursos", as: 'recursos'
   get '/articulo1', to: "pages#articulo1", as: 'articulo1'
   get '/cursos', to: "pages#cursos", as: 'cursos'
+  get '/nuevos_cursos', to: "pages#nuevos_cursos", as: 'nuevos_cursos'
   resources :lines, only: [:index, :show]
   resources :task_statuses, only: [:update]
   resources :interviews, only: [ :new, :create]
+  resources :meetings, only: [ :new, :create]
   get '/about', to: 'pages#about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
