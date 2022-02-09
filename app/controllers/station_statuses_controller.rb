8e9b1a0ea4fb3a6743166893249ebaec653bda87
Jean Pier <diegojeanpier.dc.@gmail.com>
2022-02-09 09:10:39 -0500

    quite lo que no servia

--- app/controllers/station_statuses_controller.rb
+++ app/controllers/station_statuses_controller.rb
@@ -2,12 +2,12 @@ class StationStatusesController < ApplicationController
   def update
     @station_status = StationStatus.find(params[:id])
     @station = @station_status.station #bienvenida
-    @line = @station.line  # Linea principal
     @tasks = @station.tasks #1 video
     @statuses = @tasks.map { |task| TaskStatus.where(user: current_user, task: task).first }
     @completed = @statuses.all? { |status| status.completed }
     @station_status.update( completed: @completed )
     @station_status.save
+    @line = @station.line  # Linea principal
     # respond_to do |format|
     #   if @station_status.save
     #     format.html { render 'lines/index' }
